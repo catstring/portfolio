f75fc15bd812b2966238eaeb20bbe7bdfe21f3e3
Todd MC Tsai <toddmctsai@gmail.com>
2024-07-06 01:03:48 +0800

    fix typo

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -23,7 +23,7 @@ export default function Project({ language }) {
           <p>An admin dashboard allows administrators to view and manipulate data for all users, echos, and comments. MySQL is used to manage data relationships. The responsive design is achieved using Bootstrap.</p>
         `,
         zh: `
-          <p>這是一個使用 PHP、Laravel 和 Blade 引擎開發的仿 Twitter 網站。這個專案有會員登入系統，使用者可以註冊為新用戶，使用內建密碼 Hash 功能以確保安全。在成功註冊後，會使用 Laravel Queres 和 Mail Trap 發送歡迎郵件。</p>
+          <p>這是一個使用 PHP、Laravel 和 Blade 引擎開發的仿 Twitter 網站。這個專案有會員登入系統，使用者可以註冊為新用戶，使用內建密碼 Hash 功能以確保安全。在成功註冊後，會使用 Laravel Queues 和 Mail Trap 發送歡迎郵件。</p>
           <br />
           <p>使用者可以分享、編輯和刪除他們自己的發文，稱為 "echos"，也可以追蹤其他使用者以在動態頁面上查看他們的 echos。還可以對其他使用者的 echos 進行點讚和評論，並使用搜尋框搜尋 echos。使用者可以上傳個人照片，或使用 Avataaars.io API 自動生成個人照片。Feed 頁面可以看到追蹤用戶的 echos，Top users 區顯示發表最多 echos 的用戶，並利用 cache 避免重複提取資料。</p>
           <br />
@@ -50,12 +50,12 @@ export default function Project({ language }) {
         zh: ['Laravel', 'MySQL', 'Vue.js', 'Bootstrap', 'GitHub Actions', 'AWS'],
       },
       description: {
-        en: `<p>In this project, I developed a simple task management application with a Laravel backend API and a Vite-powered Vue.js frontend. The application allows users to perform basic CRUD actions such as registering, logging in, creating tasks, editing tasks, marking them as complete, and deleting tasks. Users can also toggle the visibility of completed tasks.</p>
+        en: `<p>In this project, I developed a simple task management application with a Laravel backend Restful API and a Vite-powered Vue.js frontend. The application allows users to perform basic CRUD actions such as registering, logging in, creating tasks, editing tasks, marking them as complete, and deleting tasks. Users can also toggle the visibility of completed tasks.</p>
         <br />
-        <p>The application is hosted on an AWS VM with Nginx and secured with an HTTPS SSL certificate generated using Cerbot. CI/CD is managed with GitHub Actions.</p>`,
-        zh: `<p>這是一個簡單的代辦事項管理應用程式，後端使用 Laravel API，前端則使用 Vite 和 Vue.js。此應用程式允許使用者執行基本的 CRUD 操作，例如註冊、登入、創建任務、編輯任務、標記任務為完成，以及刪除任務。使用者還可以選擇顯示或隱藏已完成的任務。<p>
+        <p>The application is hosted on an AWS VM with Nginx and secured with an HTTPS SSL certificate generated using Certbot. CI/CD is managed with GitHub Actions.</p>`,
+        zh: `<p>這是一個簡單的代辦事項管理應用程式，採用前後端分離技術，後端 Restful API 使用 Laravel，前端則使用 Vite 和 Vue.js。此應用程式允許使用者執行基本的 CRUD 操作，例如註冊、登入、創建任務、編輯任務、標記任務為完成，以及刪除任務。使用者還可以選擇顯示或隱藏已完成的任務。<p>
         <br />
-        <p>應用程式架設在 AWS VM 上，使用 Nginx，並通過 Cerbot 生成的 HTTPS SSL 證書進行加密。持續整合和部署（CI/CD）由 GitHub Actions 管理。</p>`,
+        <p>應用程式架設在 AWS VM 上，使用 Nginx，並通過 Certbot 生成的 HTTPS SSL 認證進行加密。CI/CD 由 GitHub Actions 管理。</p>`,
       },
       images: [
         '/projects/toddoList/1.png',
